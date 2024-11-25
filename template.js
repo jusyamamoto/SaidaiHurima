fac248045d93c7e905646a0ec52724d1c62a24b9
jusyamamoto <mountainbook.397@gmail.com>
2024-11-25 14:05:39 +0900

    ヘッダーの修正

--- template.js
+++ template.js
@@ -5,10 +5,13 @@ const HTML = (body) => html`
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>これはただの文字列です</title>
+    <title>埼大フリマ</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/static/style.css">
 </head>
+<header>
+    <a href = "/">埼大フリマ</a>
+</header>
 <body>
     ${body}
 </body>
